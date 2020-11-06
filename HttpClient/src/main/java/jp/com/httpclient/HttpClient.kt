7230d852adb248e45e01b1a5008c3be2f362de79
karmakargopi <gopi.karmakar@monstar-lab.com>
2020-11-06 16:47:53 +0900

    Added Logs

--- HttpClient/src/main/java/jp/com/httpclient/HttpClient.kt
+++ HttpClient/src/main/java/jp/com/httpclient/HttpClient.kt
@@ -1,13 +1,16 @@
 package jp.com.httpclient
 
+import android.content.ClipData
+import android.util.Log
+import com.google.gson.GsonBuilder
 import okhttp3.*
 import java.io.IOException
-import com.google.gson.GsonBuilder
 
 /**
  * Created by gopi karmakar on 06,November,2020
  */
 class HttpClient {
+
 
     public fun request(url: String){
 
@@ -21,16 +24,26 @@ class HttpClient {
 
                 val gson = GsonBuilder().create()
                 val items = gson.fromJson(body, Items::class.java)
-                print(items)
+                Log.d("Rakuten", items.toString())
             }
 
             override fun onFailure(call: Call, e: IOException) {
-                print("Failed to execute")
+                Log.d("Rakuten","Failed to execute")
             }
         })
     }
 }
 
-private class Items(val items: List<Data>)
+private class Items(val items: List<Data>) {
+
+    override fun toString(): String {
+        var message: String = ""
+        items.forEach {
+            message = "Name = " + it.name + "\n" + " Privacy Status = " + it.privacy + "\n" +
+                    " Description = " + it.description + "\n" + " Language = " + it.language
+        }
+        return message
+    }
+}
 
 private class Data(val name: String, val privacy: Boolean, val description: String, val language: String)
